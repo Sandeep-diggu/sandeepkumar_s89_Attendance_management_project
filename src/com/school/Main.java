ba6c9d188c6b32ac2dd67ff8cec245f703370996
Sandeep-diggu <sandeepkumar684@gmail.com>
2025-08-28 12:45:59 +0530

    feat: Implement constructors and auto-ID generation (Part 3)

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -1,8 +1,29 @@
 package com.school;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Welcome to the School Atteendence Management System!");
-        System.out.println("Session 1: Project setup and Orientation Complete.");
-    }    
+    public static void main(String[] args) {
+        System.out.println("--- School Attendance System ---");
+
+        Student[] students = new Student[4];
+        students[0] = new Student("Alice Wonderland");
+        students[1] = new Student("Bob the builder");
+        students[2] = new Student("Charlie Chaplin");
+        students[3] = new Student("Dora Explorer");
+
+        Course[] courses = new Course[3];
+        courses[0] = new Course("Intro to programming");
+        courses[1] = new Course("Linear Algebra");
+        courses[2] = new Course("Physics 101");
+
+        System.out.println("\nRegistered Students: ");
+        for(Student student : students) {
+            if(student != null) student.displayDetails();
+        }
+
+        System.out.println("\nAvailable Courses: ");
+        for(Course course : courses) {
+            if (course != null) course.displayDetails();
+        }
+        System.out.println("\nSession 3: Constructors and Auto-ID Generation Complete");
+    }
 }
